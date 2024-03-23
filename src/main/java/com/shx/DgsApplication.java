b9e563194fce1f52e867f8c2fe22d8656032e004
hanry <hanryan658@gmail.com>
2024-03-23 16:05:03 +0800

    add gradle springBoot

---
+++ src/main/java/com/shx/DgsApplication.java
@@ -0,0 +1,11 @@
+package com.shx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DgsApplication.class,args);
+    }
+}
